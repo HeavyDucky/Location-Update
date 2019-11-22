75984191f99a68f99afed3bef3b37a9336d990cd
andrerucker <arucker@unca.edu>
2019-11-22 16:09:13 -0500

    Needs permissions from Google Api

---
+++ app/src/main/java/com/bignerdranch/android/locationupdate/MainActivity.java
@@ -0,0 +1,231 @@
+package com.bignerdranch.android.locationupdate;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Looper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String url = "jdbc:mariadb://10.123.21.91:3306/EarthDrone";
+    private static final String user = "BallardPi";
+    private static final String pass = "BallardPi";
+    private static Connection conn;
+    private static int i = 0;
+
+    boolean requestingLocationUpdates = false;
+    private FusedLocationProviderClient fusedLocationClient;
+    private LocationRequest locationRequest = new LocationRequest();
+    private LocationCallback locationCallback;
+    private Location location;
+    //private GoogleMap mMap = new GoogleMap();
+
+    Button btnFetch;
+    TextView txtData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtData = (TextView) this.findViewById(R.id.txtData);
+        btnFetch = (Button) findViewById(R.id.btnFetch);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationClient.getLastLocation();
+
+        btnFetch.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startLocationUpdates();
+                /*try {
+                    conn = DriverManager.getConnection(url, user, pass);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+                 */
+            }
+        });
+        createLocationRequest();
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                for (Location loc : locationResult.getLocations()) {
+                    // Update UI with location data
+                    // ...
+                    System.out.println(loc);
+                    location = loc;
+                    MyTask myTask = new MyTask(location);
+                    myTask.execute();
+                }
+            }
+        };
+
+        boolean permissionAccessCoarseLocationApproved =
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED;
+
+        if (permissionAccessCoarseLocationApproved) {
+            boolean backgroundLocationPermissionApproved =
+                    ActivityCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED;
+
+            if (backgroundLocationPermissionApproved) {
+                // App can access location both in the foreground and in the background.
+                // Start your service that doesn't have a foreground service type
+                // defined.
+            } else {
+                // App can only access location in the foreground. Display a dialog
+                // warning the user that your app must have all-the-time access to
+                // location in order to function properly. Then, request background
+                // location.
+                ActivityCompat.requestPermissions(this, new String[] {
+                                Manifest.permission.ACCESS_BACKGROUND_LOCATION}, 1);
+            }
+        } else {
+            // App doesn't have access to the device's location at all. Make full request
+            // for permission.
+            ActivityCompat.requestPermissions(this, new String[] {
+                            Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                    }, 1);
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (requestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+/*
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMyLocationEnabled(true);
+        mMap.setOnMyLocationButtonClickListener(this);
+        mMap.setOnMyLocationClickListener(this);
+
+    }
+
+ */
+
+
+    private void startLocationUpdates() {
+        fusedLocationClient.requestLocationUpdates(locationRequest,
+                locationCallback,
+                Looper.getMainLooper());
+        requestingLocationUpdates = true;
+    }
+
+    protected void createLocationRequest() {
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setInterval(1000);
+        locationRequest.setFastestInterval(1000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+
+// ...
+
+        SettingsClient client = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                // All location settings are satisfied. The client can initialize
+                // location requests here.
+                // ...
+            }
+        });
+
+        task.addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof ResolvableApiException) {
+                    // Location settings are not satisfied, but this can be fixed
+                    // by showing the user a dialog.
+                    try {
+                        // Show the dialog by calling startResolutionForResult(),
+                        // and check the result in onActivityResult().
+                        ResolvableApiException resolvable = (ResolvableApiException) e;
+                        resolvable.startResolutionForResult(MainActivity.this,
+                                RESULT_OK);
+                    } catch (IntentSender.SendIntentException sendEx) {
+                        // Ignore the error.
+                    }
+                }
+            }
+        });
+    }
+
+    public final class MyTask extends AsyncTask<String,Void,String>{
+
+        Location location;
+
+        private MyTask(Location location) {
+            this.location = location;
+    //        txtData.setText("Database is being updated");
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            try {
+                conn = DriverManager.getConnection(url, user, pass);
+
+                i++;
+                Statement preparedStmt = conn.createStatement();
+                preparedStmt.execute(" Update Test Set Battery='" + i + "' Where Row = '1'");
+                preparedStmt.execute(" Update Test Set Heading=" + location.getBearing() + " Where Row = '1'");
+                preparedStmt.execute(" Update Test Set Lon='" + location.getLongitude() + "' Where Row = '1'");
+                preparedStmt.execute(" Update Test Set Lat='" + location.getLatitude() + "' Where Row = '1'");
+
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        protected void onPostExecute(String result) {
+            txtData.setText("Database was updated with new coordinates\n" + i);
+        }
+
+    }
+}
